47543fae00308252c130115e5afc7c690cdab821
Matthew Crompton <crompton.ml@gmail.com>
2021-05-07 11:12:02 +0200

    Add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,60 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Orders from "./pages/Orders";
 
 const App = () => {
- const [user, setUser] = useState("");
+ const [orders, setOrders] = useState();
+ const [textVal, setTextVal] = useState("");
+ const [isLoading, setIsLoading] = useState(false);
 
- //  useEffect(() => {
- //   const postTest = async () => {
- //    const res = await fetch("/checkpoint", {
- //     method: "POST",
- //     headers: {
- //      "Content-Type": "application/json",
- //     },
- //     body: JSON.stringify({ tracking_number: "00340000161200000001" }),
- //    });
- //    const data = await res.json();
- //    console.log(data);
- //   };
- //   postTest();
- //  }, []);
- return <div className="App"></div>;
+ const getOrders = async (email = "julian@parcellab.com") => {
+  try {
+   const res = await fetch("/orders", {
+    method: "POST",
+    headers: {
+     "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+   });
+   const data = await res.json();
+   setOrders(data);
+   setIsLoading(false);
+  } catch (e) {
+   console.warn(e);
+   setIsLoading(false);
+  }
+ };
+
+ const handleSubmit = () => {
+  setIsLoading(true);
+  getOrders(textVal);
+  setTextVal("");
+ };
+
+ if (orders) {
+  return <Orders orders={orders} />;
+ }
+ return (
+  <div className="App">
+   <p>Please enter your email address to see your recent orders</p>
+   {isLoading ? (
+    <div>Loading...</div>
+   ) : (
+    <div>
+     <div className="input-label">Email</div>
+     <input
+      type="text"
+      value={textVal}
+      onChange={(e) => setTextVal(e.target.value)}
+      disabled={isLoading}
+     />
+     <button disabled={isLoading || !textVal} onClick={handleSubmit}>
+      Submit
+     </button>
+    </div>
+   )}
+  </div>
+ );
 };
 
 export default App;
