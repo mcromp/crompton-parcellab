fbe00a980564ddcd94886a32fbf65c57e9821fb5
Matthew Crompton <crompton.ml@gmail.com>
2021-05-07 09:42:41 +0200

    Add json data as dummydb

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var checkpoints = require("../dummydb/checkpoints.json");
+var trackings = require("../dummydb/trackings.json");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+ res.render("index", { title: "Express" });
 });
 
 module.exports = router;
