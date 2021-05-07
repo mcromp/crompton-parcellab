074c0032f990d46cfa9fa8a700ce6d2581583069
Matthew Crompton <crompton.ml@gmail.com>
2021-05-07 10:45:46 +0200

    Remove client boilerplate and init refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
- useEffect(() => {
-  const postTest = async () => {
-   const res = await fetch("/checkpoint", {
-    method: "POST",
-    headers: {
-     "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ tracking_number: "00340000161200000001" }),
-   });
-   const data = await res.json();
-   console.log(data);
-  };
-  postTest();
- }, []);
+ const [user, setUser] = useState("");
+
+ //  useEffect(() => {
+ //   const postTest = async () => {
+ //    const res = await fetch("/checkpoint", {
+ //     method: "POST",
+ //     headers: {
+ //      "Content-Type": "application/json",
+ //     },
+ //     body: JSON.stringify({ tracking_number: "00340000161200000001" }),
+ //    });
+ //    const data = await res.json();
+ //    console.log(data);
+ //   };
+ //   postTest();
+ //  }, []);
  return <div className="App"></div>;
 };
 
