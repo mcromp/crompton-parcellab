ce7fb141aab754eed293ebf2b4e4fa02627a7cb3
Matthew Crompton <crompton.ml@gmail.com>
2021-05-07 10:00:50 +0200

    Add tracking post get

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,20 @@ var checkpoints = require("../dummydb/checkpoints.json");
 var trackings = require("../dummydb/trackings.json");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
- res.render("index", { title: "Express" });
+router.get("/", function (req, res) {
+ console.log(checkpoints);
+
+ res.json(checkpoints);
+});
+router.post("/trackings", function (req, res) {
+ const { email } = req.body;
+ if (email) {
+  const userTrackings = trackings.filter((track) => track.email === email);
+  res.status(200).json(userTrackings);
+ }
+ if (email === "") {
+  res.status(400).json("empty");
+ }
 });
 
 module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+ useEffect(() => {
+  const postTest = async () => {
+   const res = await fetch("/trackings", {
+    method: "POST",
+    headers: {
+     "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: "julian@parcellab.com" }),
+   });
+   const data = await res.json();
+   console.log(data);
+  };
+  postTest();
+ }, []);
+ return <div className="App"></div>;
+};
 
 export default App;
