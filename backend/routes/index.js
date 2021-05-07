91fc7790d5346cd9e142f3e576b5f6cd9780bb75
Matthew Crompton <crompton.ml@gmail.com>
2021-05-07 10:37:44 +0200

    Add checkpoint post to backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,23 +1,39 @@
 var express = require("express");
 var router = express.Router();
-var checkpoints = require("../dummydb/checkpoints.json");
-var trackings = require("../dummydb/trackings.json");
+var checkpointsDB = require("../dummydb/checkpoints.json");
+var trackingsDB = require("../dummydb/trackings.json");
+
+const compareDates = (a, b) => {
+ const date1 = new Date(a);
+ const date2 = new Date(b);
+ //return date1 if newer
+ return date1.getTime() > date2.getTime();
+};
 
 /* GET home page. */
-router.get("/", function (req, res) {
- console.log(checkpoints);
-
- res.json(checkpoints);
+router.get("/", (req, res) => {
+ res.status(200).json(checkpointsDB);
 });
-router.post("/trackings", function (req, res) {
+
+router.post("/orders", (req, res) => {
  const { email } = req.body;
- if (email) {
-  const userTrackings = trackings.filter((track) => track.email === email);
-  res.status(200).json(userTrackings);
- }
- if (email === "") {
-  res.status(400).json("empty");
- }
+ if (email === "") res.status(400).json("empty");
+ const userTrackings = trackingsDB.filter((track) => track.email === email);
+ res.status(200).json(userTrackings);
+});
+
+router.post("/checkpoint", (req, res) => {
+ const { tracking_number } = req.body;
+ const checkpoints = checkpointsDB.filter(
+  (c) => c.tracking_number === tracking_number,
+ );
+ if (!checkpoints) res.status(404);
+ //returns the latest checkpoint for requested order
+ const latestCheckpoint = checkpoints.reduce((acc, cur) => {
+  if (compareDates(cur.timestamp, acc.timestamp)) return cur;
+  return acc;
+ }, checkpoints[0]);
+ res.status(200).json(latestCheckpoint);
 });
 
 module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import { useEffect } from "react";
 const App = () => {
  useEffect(() => {
   const postTest = async () => {
-   const res = await fetch("/trackings", {
+   const res = await fetch("/checkpoint", {
     method: "POST",
     headers: {
      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email: "julian@parcellab.com" }),
+    body: JSON.stringify({ tracking_number: "00340000161200000001" }),
    });
    const data = await res.json();
    console.log(data);
